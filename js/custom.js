55e0041136dad9d9b727d525c66daaf3f5681b39
Andy Standfield <vertigo25@gmail.com>
2013-10-13 21:52:41 -0700

    First pages commit

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$('dl.single_roll').click(function() {
+		$('dl.single_roll.on .manipulator').fadeToggle(200);
+		$('.manipulator',this).fadeToggle(200);
+		$('dl.single_roll.on').removeClass('on');
+		$(this).toggleClass('on');
+		event.stopPropagation();
+	});
+
+
+
+	$('.manipulator .button').click(function() {
+		console.log($(this).html());
+	});
+
+	$('html').click(function() {
+		if ($('dl.single_roll').hasClass('on')) {
+			$('dl.single_roll.on .manipulator').fadeToggle(200);
+			$('dl.single_roll.on').removeClass('on');
+		}
+	});
+
+});
